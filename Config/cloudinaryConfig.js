209243c5102a0cdba6eefd1b7bfd1765ea1045be
Tushar <tusharvarshney1810@gmail.com>
2024-12-03 14:51:18 +0530

    Added Dotenv in cloudconfiq

--- Config/cloudinaryConfig.js
+++ Config/cloudinaryConfig.js
@@ -1,4 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load .env file
+
 
 // Automatically uses CLOUDINARY_URL from the environment
 cloudinary.config();
